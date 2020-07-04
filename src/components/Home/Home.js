ecae4a5e6f1553857ce25c451f8b5b56c658a0e0
Gilberto Suarez <gilbertosuarez@pop-os.localdomain>
2020-07-04 10:33:06 -0400

    feat(home): add test home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Home.scss";
+
+function Home() {
+  return (
+    <main className="home__container">
+      <h1>Hello World</h1>
+    </main>
+  );
+}
+
+export default Home;
