a76167649f7108a60cf0a10646e6cbdf4fbfacb8
Gilberto Suarez <suarezgilberto@hotmail.com>
2020-07-11 12:53:46 -0400

    chore(app): add css to react-slick

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-props-no-spreading */
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
