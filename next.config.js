45f83cdbcc89dc94393c52f51f49d9e22266d7b8
Gilberto Suarez <suarezgilberto@hotmail.com>
2020-07-12 19:21:07 -0400

    chore: add config to allow svg

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
   assetPrefix: !debug ? "/pokeapp/" : "",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.jsx?$/,
+      },
+      use: ["@svgr/webpack", "url-loader"],
+    });
+
+    return config;
+  },
 };
